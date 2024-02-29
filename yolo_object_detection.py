cadd960d6d838a5e730acb787783233a3df5e014
Bonheur_NE <bonheurndezenc@gmail.com>
2024-02-29 15:54:53 +0200

    object_detection # generate a JSON file with object names (if available) and the number of objects detected

--- yolo_object_detection.py
+++ yolo_object_detection.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import json
 from yolo_setup import *
 
 def perform_object_detection(image_path, output_path):
@@ -67,8 +68,25 @@ def perform_object_detection(image_path, output_path):
     # Save annotated image
     cv2.imwrite(output_path, image)
 
+    # generate a json file containing the needed data
+    generate_json("data/output_data.json", class_ids, classes)
+
     # print object count
     print(f"Number of objects detected: {object_count}")
 
+
+def generate_json(output_path, class_ids, classes):
+    # count the number of occurences of each class
+    class_count = {}
+    for class_id in class_ids:
+        class_name =  classes[class_id]
+        class_count[class_name] = class_count.get(class_name, 0) + 1
+
+    # convert the class count dictionnary to JSON format
+    json_data = json.dumps(class_count, indent=4)
+
+    # write JSON data to file
+    with open(output_path, 'w') as json_file:
+        json_file.write(json_data)    
 # Call object detection function
 perform_object_detection("images/highway-traffic.jpg", "images/annotated-image.jpg")
